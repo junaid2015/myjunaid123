bb5ce1a6779749a0f320c6861f68d8e1afcd9f97
junaid2015 <152391441+junaid2015@users.noreply.github.com>
2023-11-29 18:39:03 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,82 @@
+# def calculate():
+#     a=10
+#     b=20
+#     print((a+b)/(a*b))
+
+# calculate()   
+
+
+# def calculate(a,b):
+    
+#     print((a+b)/(a*b))
+# a=10
+# b=20    
+
+# calculate(a,b)  
+
+
+# def calculate(a,b):
+    
+#     print((a+b)/(a*b))
+# def iscompare(a,b):
+#  if(a>b):
+#     print("a is greater than b")
+#  else:
+#     print("b is greater than a")
+
+# def islesser(a,b):
+#    pass
+# a=10
+# b=20
+# iscompare(a,b)
+# calculate(a,b)  
+
+# Default functions
+# def average(a=10,b=20):
+#     print((a+b)/2)
+# average()
+
+# def average(a=10,b=20):
+#     print((a+b)/2)
+# average(a=16)
+
+# def average(a=10,b=20):
+#     print((a+b)/2)
+# average(a=16,b=32)
+
+
+# keyword argument
+# def average(a=10,b=20):
+#     print((a+b)/2)
+# average(b=16,a=32)
+
+
+# required argument
+# def average(a,b,c=9):
+#     print((a+b+c)/3)
+
+# average(5,10)
+
+
+#variable length argument
+
+# def average(*numbers):
+#     sum=0
+#     for i in numbers:
+#         sum=sum+i
+#     print("the average is ", sum/len(numbers))
+
+# average(10,15,10,20)
+
+
+# def average(*numbers):
+#     sum=0
+#     for i in numbers:
+#         sum=sum+i
+#     return sum/ len(numbers)
+
+# c = average(10,15,10,20)  #default parameters
+# print(c)
+
+
+
